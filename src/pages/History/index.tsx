bc6811f89a41e196e6de135f0b6e70b57d90f680
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-10 18:06:09 -0300

    Initial commit

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,71 @@
+import { useContext } from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { HistoryContainer, HistoryList, Status } from './styles';
+import { CyclesContext } from '../../contexts/CyclesContext';
+
+export function History() {
+  const { cycles } = useContext(CyclesContext);
+
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      {/* <pre>
+        {JSON.stringify(cycles, null, 2)}
+      </pre>  para visualizar o objeto cycles */}
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor='green'>Concluído</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor='red'>Interrompido</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor='yellow'>Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              {' '}
+              {/**Exemplo estático: */}
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
